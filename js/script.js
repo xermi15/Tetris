6ea181841c50ef38546c05649576d2d528caf359
Xavier Franch <43445674+xermi15@users.noreply.github.com>
2018-12-05 18:26:12 +0100

    v0.1 tasca 1 acabada

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
-var joc
+var estatActual = function(){
+    taulerActual[0] = new Array(25);
+            taulerActual[0] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[1] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[2] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[3] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[4] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[5] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[6] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[7] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[8] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[9] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[10] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[11] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[12] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[13] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[14] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[15] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[16] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[17] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[18] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[19] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[20] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[21] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[22] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[23] = [0,0,0,0,0,0,0,0,0,0];
+            taulerActual[24] = [0,0,0,0,0,0,0,0,0,0];
+
+    estatTauler: 1,
+    puntuacio: 0,
+    puntuacioMax: 0,
+    pesaActual: "new Object",
+    pesaSeguent: "new Object",
+    comptadorPesa: new Array(7),
+    interval: 1000;
+
+    iniciarJoc: function(){}
+    obtenirNovaPesa: function(){}
+    tecla: function(){}
+    movimentAuto: function(interval){}
+}
+
